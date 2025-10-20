395031168c3b62e89b203a57bd8e662bd3e2614b
Risyad Rais <satmo54@gmail.com>
2025-10-20 10:34:01 +0700

    lets go back being static lol

--- app/Services/EndTransactionService.php
+++ app/Services/EndTransactionService.php
@@ -7,7 +7,7 @@ class EndTransactionService
     /**
      * Create a new class instance.
      */
-    public function handle($node_id, $from)
+    public static function handle($node_id, $from)
     {
         $transactions = IoTChain::query()
             ->where('transaction->node', '=', $node_id)
